b6e04b5208895d75e2cf83d46a1882aae63fd804
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-01-25 20:28:53 +0530

    Created left container, right container and also worked on right container part

---
+++ weather-app/src/app/right-container/right-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-container',
+  imports: [],
+  templateUrl: './right-container.component.html',
+  styleUrl: './right-container.component.scss'
+})
+export class RightContainerComponent {
+
+}

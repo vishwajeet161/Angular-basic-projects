592f5096143145cea363bba7b9121b584828e3ad
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-01-27 22:59:16 +0530

    Completed the work on left container nav bar

--- weather-app/src/app/left-container/left-container.component.ts
+++ weather-app/src/app/left-container/left-container.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-left-container',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './left-container.component.html',
   styleUrl: './left-container.component.scss'
 })
 export class LeftContainerComponent {
+  //Variables for font awesome icons
 
+  //Variables for left-nav-bar search icons
+  faMagnifyingGlass:any = faMagnifyingGlass;
+  faLocation:any = faLocation;
 }
--- weather-app/src/app/right-container/right-container.component.ts
+++ weather-app/src/app/right-container/right-container.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './right-container.component.scss'
 })
 export class RightContainerComponent {
+  //variables to control tabs
+  today:boolean = false;
+  week:boolean = true;
 
+  //variables to control metric values
+  celsius:boolean = true;
+  fahrenhiet:boolean = false;
+
+  //functions to control tab values or tab states
+
+  //function to click on tab today
+  onTodayTabClick(){
+    this.today = true;
+    this.week = false;
+  }
+
+  //functiom to click on tab week
+  onWeekTabClick(){
+    this.today = false;
+    this.week = true;
+  }
+
+  //functions to control metric values
+
+  //function for click on celsius metric
+  onCelsiusClick(){
+    this.celsius = true;
+    this.fahrenhiet = false;
+  }
+
+  //functions for click on fahrenhiet metric
+  onFahrenheitClick(){
+    this.celsius = false;
+    this.fahrenhiet = true;
+  }
 }

27e5fd5ea229242a383cac789aa4164c4530e1ad
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-02-09 17:06:56 +0530

    Started working on backend part, created Models

---
+++ weather-app/src/app/Models/TemperatureData.ts
@@ -0,0 +1,10 @@
+
+export interface TemperatureData {
+    temperature: number;
+    summaryImage:string;
+    day: String;
+    time: String;
+    summaryPhrase: string;
+    rainPercent: number;
+    location:string;
+}
---
+++ weather-app/src/app/Models/TodayData.ts
@@ -0,0 +1,6 @@
+export interface TodayData {
+    time:string;
+    summaryImage: string;
+    temperature: number;
+    
+}

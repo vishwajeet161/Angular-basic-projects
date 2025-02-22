1381628a983123a9323443a670ccfa8a6bf3bcdb
vishwajeet161 <vishwajeet.patel161@gmail.com>
2025-02-22 16:45:12 +0530

    Fetched the data from api for today's and week's weather forcast

--- weather-app/src/app/Services/weather.service.ts
+++ weather-app/src/app/Services/weather.service.ts
@@ -21,8 +21,8 @@ export class WeatherService {
   //Variables that have the extracted data from the API Endpoint Variables
   temperatureData: TemperatureData = new TemperatureData(); // Left-container Data
 
-  todayData?: TodayData;//Right-container Data
-  weekData?: WeekData;// Right-container Data
+  todayData?: TodayData[] = [];//Right-container Data
+  weekData?: WeekData[] = [];// Right-container Data
   todaysHighlight?: TodaysHighlight;// Right-container Data
 
 
@@ -36,11 +36,16 @@ export class WeatherService {
   currentTime:Date;
   constructor(private httpClient: HttpClient) {
     this.getData();
+
   }
 
   getSummaryImage(summary: string): string{
+    //Base folder address containing the images
     var baseAddress = "\\home\\vishwajeet161\\Desktop\\angular\\Angular-basic-projects\\weather-app\\public\\assets\\";
+
+    //respective image names
     var cloudySunny = "cloudy-sunny.png";
+    var rainSunny = "rainy-and-sunny.png";
     var rainy = "rainy.png";
     var windy = "windy.png";
     var sunny = "sunny.png";
@@ -48,12 +53,19 @@ export class WeatherService {
     var cloudy = "cloudy.png";
     var snow = "snow.png";
     var thunderstorm = "thunderstorm.png";
-    return "";
+
+    if(String(summary).includes("Partly Cloudy") || String(summary).includes("P Cloudy"))return baseAddress+cloudySunny;
+    else if(String(summary).includes("Partly Rainy") || String(summary).includes("P Rainy"))return baseAddress+rainSunny;
+    else if(String(summary).includes("wind"))return baseAddress+windy;
+    else if(String(summary).includes("rain"))return baseAddress+rainy;
+    else if(String(summary).includes("sun"))return baseAddress+sunny;
+
+    return baseAddress+cloudySunny;
   }
 
-  //Method to create useful data chunks for UI using the data recieved from API
-  prepareData():void {
-    //Setting Left container Data Model Properties
+  //Method to create a chunk for left container using model TemperatureData
+  fillTemperatureDataModel(){
+     //Setting Left container Data Model Properties
     this.currentTime = new Date();
     this.temperatureData.day = this.weatherDetails['v3-wx-observations-current'].dayOfWeek;
     this.temperatureData.time = `${this.currentTime.getHours()}:${this.currentTime.getMinutes()}`;
@@ -61,6 +73,41 @@ export class WeatherService {
     this.temperatureData.location = `${this.locationDetails.location.city[0]}, ${this.locationDetails.location.country[0]}`;
     this.temperatureData.rainPercent = this.weatherDetails['v3-wx-observations-current'].precip24Hour;
     this.temperatureData.summaryPhrase = this.weatherDetails['v3-wx-observations-current'].wxPhraseShort;
+  }
+
+  //Method to create a chunk for left container using model TemperatureData
+  fillWeekData(){
+    var weekCount = 0;
+    while(weekCount < 7){
+    this.weekData.push(new WeekData());
+    this.weekData[weekCount].day = this.weatherDetails['v3-wx-forecast-daily-15day'].dayOfWeek[weekCount].slice(0,3);
+    this.weekData[weekCount].tempMax = this.weatherDetails['v3-wx-forecast-daily-15day'].calendarDayTemperatureMax[weekCount].toString();
+    this.weekData[weekCount].tempMin = this.weatherDetails['v3-wx-forecast-daily-15day'].calendarDayTemperatureMin[weekCount].toString();
+    this.weekData[weekCount].summaryImage = this.getSummaryImage(this.weatherDetails['v3-wx-forecast-daily-15day'].narrative[weekCount]);
+    weekCount++;
+    }
+  }
+
+  fillTodayData(){
+    var todayCount = 0;
+    while(todayCount < 7){
+      this.todayData.push(new TodayData());
+      this.todayData[todayCount].time = this.weatherDetails['v3-wx-forecast-hourly-10day'].validTimeLocal[todayCount].slice(11,16);
+      this.todayData[todayCount].temperature = this.weatherDetails['v3-wx-forecast-hourly-10day'].temperature[todayCount];
+      this.todayData[todayCount].summaryImage = this.getSummaryImage(this.weatherDetails['v3-wx-forecast-hourly-10day'].wxPhraseShort[todayCount]);
+      todayCount++;
+
+    }
+  }
+  
+  //Method to create useful data chunks for UI using the data recieved from API
+  prepareData():void {
+    this.fillTemperatureDataModel();
+    this.fillWeekData();
+    this.fillTodayData();
+    console.log("Week Data:",this.weekData);
+    console.log("Today Data:",this.todayData);
+    console.log("Temperature Data:",this.temperatureData);
   }
 
 
